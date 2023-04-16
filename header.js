e3a55b949e1d5af26ccb9b24703fd6058f13b17d
alok <rickey.alok@gmail.com>
2023-04-16 22:13:28 +0530

    updated

---
+++ header.js
@@ -0,0 +1,19 @@
+header.innerHTML = `
+<nav>
+  <div>
+    <a href="../index.html">Home</a>
+    <a href="./pages/gitGitHub.html">GitGitHub</a>
+    <a href="./pages/Markdown.html">Markdown</a>
+    <a href="./pages/JSON.html">Json</a>
+    <a href="./pages/MSON.html">Mson</a>
+    <a href="./pages/CURL.html">Curl</a>
+    <a href="./pages/YAML.html">Yaml</a>
+    <a href="./pages/HTTP-HTTPS.html">Http-Https</a>
+    <a href="./pages/OpenAPI.html">OpenAPI</a>
+    <a href="./pages/OSI-Model.html">OSI-Model</a>
+    <div id="header-right">
+      <a href="./pages/contact.html">Contact</a>
+    </div>
+  </div>
+</nav>
+`;
