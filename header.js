bad79b115a87a72428e9b37c48e8bf922cc745fe
alok <rickey.alok@gmail.com>
2023-04-16 22:22:40 +0530

    updated

--- header.js
+++ header.js
@@ -1,7 +1,7 @@
 header.innerHTML = `
 <nav>
   <div>
-    <a href="../index.html">Home</a>
+    <a href="./index.html">Home</a>
     <a href="./pages/gitGitHub.html">GitGitHub</a>
     <a href="./pages/Markdown.html">Markdown</a>
     <a href="./pages/JSON.html">Json</a>
@@ -12,7 +12,7 @@ header.innerHTML = `
     <a href="./pages/OpenAPI.html">OpenAPI</a>
     <a href="./pages/OSI-Model.html">OSI-Model</a>
     <div id="header-right">
-      <a href="./pages/contact.html">Contact</a>
+      <a href="./contact.html">Contact</a>
     </div>
   </div>
 </nav>
