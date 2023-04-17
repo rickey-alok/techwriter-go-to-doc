265a80eecd83e8029e03d8b9b4a800992e4c0666
alok <rickey.alok@gmail.com>
2023-04-17 20:49:24 +0530

    updated

--- header.js
+++ header.js
@@ -2,7 +2,8 @@ header.innerHTML = `
   <nav>
     <div>
       <a href="./index.html">Home</a>
-      <a href="./pages/gitGitHub.html">GitGitHub</a>
+      <a href="./pages/gitGitHub.md">GitGitHub</a>
+      <a href="./gitGitHub.html">GitGitHubNew</a>
       <a href="./pages/Markdown.html">Markdown</a>
       <a href="./pages/JSON.html">Json</a>
       <a href="./pages/MSON.html">Mson</a>
